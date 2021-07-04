43f9cfeb102e21ecd61d93916acadc527c7a9578
rayferric <63957587+rayferric@users.noreply.github.com>
2021-07-04 21:23:40 +0200

    Rename hash.json to packages.json.sha256

--- index_keywords.py
+++ index_keywords.py
@@ -31,5 +31,5 @@ json_string = json.dumps(sorted_keywords, sort_keys=True, indent='\t')
 with open('packages.json', 'w') as file:
     file.write(json_string)
 
-with open('hash.json', 'w') as file:
-	json.dump(hashlib.sha256(json_string.encode()).hexdigest(), file)
+with open('packages.sha256', 'w') as file:
+	file.write(hashlib.sha256(json_string.encode()).hexdigest())

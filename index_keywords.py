6cf7c7f23f6f406919a763f00eb2bf7766bb7622
rayferric <63957587+rayferric@users.noreply.github.com>
2021-07-04 06:19:57 +0200

    Add proper keyword indexing

--- index_keywords.py
+++ index_keywords.py
@@ -1,3 +1,30 @@
-file = open('indexer-hello.txt', 'w')
-file.write('Hello from keyword indexer!\n')
-file.close()
+import os, json
+
+packages_dir = './packages/'
+
+keywords = {}
+
+def add_package_keyword(package_name, keyword):
+	if keyword in keywords:
+		keywords[keyword].add(package_name)
+	else:
+		keywords[keyword] = { package_name }
+
+for filename in os.listdir(packages_dir):
+	package_name = os.path.splitext(filename)[0]
+
+	with open(os.path.join(packages_dir, filename), 'r') as file:
+		for keyword in package_name.split('-'):
+			add_package_keyword(package_name, keyword)
+		for keyword in json.load(file)['keywords']:
+			add_package_keyword(package_name, keyword)
+
+sorted_keywords = [{
+	'keyword': keyword,
+	'packages': list(keywords[keyword])
+} for keyword in keywords]
+
+sorted_keywords.sort(key=lambda x : x['keyword'])
+
+with open('keywords.json', 'w') as file:
+    json.dump(sorted_keywords, file, indent='\t')

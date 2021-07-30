71e94c06d1a5949527cd7ae5559926ad6f31a7cf
rayferric <63957587+rayferric@users.noreply.github.com>
2021-07-30 17:11:57 +0200

    Support /top route when top.json is missing.

--- src/routers/top.js
+++ src/routers/top.js
@@ -2,14 +2,25 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 
+import lock from '../util/lock.js';
 import config from '../config.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-	const filepath = path.join(config.dataPath, '/top.json');
+	const filepath = path.normalize(path.join(config.dataPath, '/top.json'));
+
+	let data = undefined;
+	await lock.acquire(filepath, async () => {
+		try {
+			data = await fs.promises.readFile(filepath, 'utf-8');
+		} catch {
+			data = '[]';
+		}
+	});
+
 	res.set('Content-Type', 'application/json');
-	res.send(await fs.promises.readFile(filepath, 'utf-8'));
+	res.send(data);
 });
 
 export default router;
